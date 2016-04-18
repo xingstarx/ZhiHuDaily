a76067b8d05a1de44bce9d5d18f8ca04c2e95a9d
xingxing <xxx823952375@gmail.com>
2016-04-18 23:50:56 +0800

    set ThemeItemFragment content view resource layout

--- app/src/main/java/com/example/star/zhihudaily/ThemeItemFragment.java
+++ app/src/main/java/com/example/star/zhihudaily/ThemeItemFragment.java
@@ -66,7 +66,7 @@ public class ThemeItemFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_theme, container, false);
         initViews(view);
         return view;
     }
